819260b1c8501bc3e4eacb141eb5b0b7d4dd6fbb
SupaMega24 <138974329+SupaMega24@users.noreply.github.com>
2024-04-18 21:53:51 +0900

    Add custom not found

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,20 @@
+
+import { notFound } from "next/navigation";
+
 export default function ProductReviews({ params }: {
     params: {
         productId: string;
         reviewId: string;
     }
 }) {
-    return <h1>Review {params.reviewId} of Product {params.productId}</h1>
+
+    if (parseInt(params.reviewId) > 1000) {
+        notFound(); // from next navigation
+    }
+
+    return (
+        <h1>
+            Review {params.reviewId} of Product {params.productId}
+        </h1>
+    );
 }
